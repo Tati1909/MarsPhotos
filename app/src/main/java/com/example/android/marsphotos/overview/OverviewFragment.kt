45150592343c725db452674108d767c2c3abdc41
татьяна <emuart155@gmail.com>
2021-10-02 15:58:24 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android/marsphotos/overview/OverviewFragment.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.marsphotos.overview
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.android.marsphotos.databinding.FragmentOverviewBinding
+
+/**
+ * This fragment shows the the status of the Mars photos web services transaction.
+ */
+class OverviewFragment : Fragment() {
+
+    private val viewModel: OverviewViewModel by viewModels()
+
+    /**
+    Раздувает макет с помощью binding, устанавливает его владельца жизненного цикла на OverviewFragment,
+    чтобы включить binding для наблюдения за LiveData, и настраивает RecyclerView с адаптером.
+     */
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentOverviewBinding.inflate(inflater)
+
+        //Разрешает привязке данных наблюдать за LiveData в течение жизненного цикла этого фрагмента
+        binding.lifecycleOwner = this@OverviewFragment
+
+       //Предоставляем привязке доступ к OverviewViewModel
+        binding.viewModel = viewModel
+
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.marsphotos.overview
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+
+class OverviewViewModel : ViewModel() {
+
+    //Внутренняя MutableLiveData, в котором хранится статус самого последнего запроса
+    private val _status = MutableLiveData<String>()
+
+    // Внешняя неизменяемая LiveData для статуса запроса
+    val status: LiveData<String> = _status
+
+    /**
+    Вызов getMarsPhotos() при инициализации, чтобы мы могли немедленно отобразить статус.
+     */
+    init {
+        getMarsPhotos()
+    }
+
+    /**
+    Получаем информацию о фотографиях Марса из службы Retrofit Mars API и обновляем [MarsPhoto] [List] [LiveData].
+     */
+    private fun getMarsPhotos() {
+        _status.value = "Set the Mars API status response here!"
+    }
+}
